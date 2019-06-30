8c5fdc445b37e09d8b1206f3fbbcd6081d7cd7e0
gradzio <gradzio@bettercollective.com>
2019-06-30 02:55:58 +0200

    Added jasmine and solved first exercise

---
+++ src/6kyu/alphabetized.spec.js
@@ -0,0 +1,27 @@
+import { alphabetized } from "./alphabetized";
+
+describe('Alphabetized', () => {
+  it('should return empty', () => {
+    expect(alphabetized('')).toEqual('');
+  })
+
+  it('should sort single word', () => {
+    expect(alphabetized('dcba')).toEqual('abcd');
+  })
+
+  it('should sort multiple words', () => {
+    expect(alphabetized('bc da')).toEqual('abcd');
+  })
+
+  it('should handle single word with capital letter', () => {
+    expect(alphabetized('BbaAdD')).toEqual('aABbdD');
+  })
+
+  it('should handle multiple words with capitals', () => {
+    expect(alphabetized('bB Aa dD Cc')).toEqual('AabBCcdD');
+  })
+
+  it('should handle non letters', () => {
+    expect(alphabetized(' ,.!@#b$%a')).toEqual('ab');
+  })
+})
