074a370fdb55311567624a420f7b13545923e839
gradzio <gregradzio@gmail.com>
2021-01-10 13:44:53 +0100

    finished tortoise race

---
+++ src/6kyu/tortoise-racing.spec.js
@@ -0,0 +1,15 @@
+function race(v1, v2, g) {
+    if (v1 >= v2) {
+        return null
+    }
+    let diff = g / (v2 - v1)
+    var n = new Date(0,0)
+    n.setSeconds(+diff * 60 * 60)
+   return n.toTimeString().slice(0, 8).split(':').map(str => Math.floor(str))
+}
+
+fdescribe('race', () => {
+  it('basic test', () => {
+    expect(race(720, 850, 70)).toEqual([0, 32, 18])
+  })
+})
